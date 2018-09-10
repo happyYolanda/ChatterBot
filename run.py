5005dc39e09634adedb585a93ac7dc00b59c68ef
Jing Ma <majingmj@rd.netease.com>
2018-09-10 12:43:15 +0800

    add run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+from chatterbot import ChatBot
+
+chatbot = ChatBot(
+	'Ron Obvious',
+	trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+)
+
+def main():
+	prompt = "Please enter the question:\n"
+	while True:
+		sentence = raw_input(prompt)
+		if sentence == 'quit':
+			break
+		else:
+			response = chatbot.get_response(sentence)
+			print (response + '\n')
+
+if __name__ == "__main__":
+	main()
